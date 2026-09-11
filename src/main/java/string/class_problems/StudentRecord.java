f9f7d4baf55c8f2131c8f1bad899533e06e16fe0
Deep Jain <deepbhai094@gmail.com>
2026-09-11 18:36:11 +0530

    Refactor StudentRecord to handle CSV input
    Refactored StudentRecord class to parse student records from CSV input and removed the Rock-Paper-Scissors game logic.

--- src/main/java/string/class_problems/StudentRecord.java
+++ src/main/java/string/class_problems/StudentRecord.java
@@ -1,80 +1,29 @@
-package string.class_problems;
+import java.util.Scanner;
 
-import java.util.*;
+public class Studentrecord {
 
-public class RPS {
+    static void parseStudentRecord(String csvLine) {
 
-    static String playRound(String playerMove, String computerMove) {
+        String[] data = csvLine.split(",");
 
-        if (playerMove.equals(computerMove)) {
-            return "Draw";
+        if (data.length != 3) {
+            System.out.println("Invalid Record");
+            return;
         }
 
-        if ((playerMove.equals("Rock") && computerMove.equals("Scissors")) ||
-            (playerMove.equals("Paper") && computerMove.equals("Rock")) ||
-            (playerMove.equals("Scissors") && computerMove.equals("Paper"))) {
-            return "Player Wins";
-        }
-
-        return "Computer Wins";
+        System.out.println("Name: " + data[0]);
+        System.out.println("Roll No: " + data[1]);
+        System.out.println("Dept: " + data[2]);
     }
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        Random random = new Random();
 
-        String[] moves = {"Rock", "Paper", "Scissors"};
+        System.out.print("Enter student record: ");
+        String csvLine = sc.nextLine();
 
-        int wins = 0;
-        int losses = 0;
-        int draws = 0;
-
-        String[] playerMoves = new String[5];
-        String[] computerMoves = new String[5];
-        String[] results = new String[5];
-
-        for (int i = 0; i < 5; i++) {
-
-            System.out.print("Enter Rock, Paper or Scissors: ");
-            String playerMove = sc.next();
-
-            String computerMove = moves[random.nextInt(3)];
-
-            String result = playRound(playerMove, computerMove);
-
-            playerMoves[i] = playerMove;
-            computerMoves[i] = computerMove;
-            results[i] = result;
-
-            System.out.println("Computer: " + computerMove);
-            System.out.println("Result: " + result);
-            System.out.println();
-
-            if (result.equals("Player Wins"))
-                wins++;
-            else if (result.equals("Computer Wins"))
-                losses++;
-            else
-                draws++;
-        }
-
-        System.out.println("----- FINAL SUMMARY -----");
-        System.out.println("Round\tPlayer\tComputer\tResult");
-
-        for (int i = 0; i < 5; i++) {
-            System.out.println((i + 1) + "\t" +
-                    playerMoves[i] + "\t" +
-                    computerMoves[i] + "\t" +
-                    results[i]);
-        }
-
-        double winPercentage = (wins * 100.0) / 5;
-
-        System.out.println("\nWins: " + wins);
-        System.out.println("Losses: " + losses);
-        System.out.println("Draws: " + draws);
-        System.out.println("Win %: " + winPercentage + "%");
+        parseStudentRecord(csvLine);
 
         sc.close();
     }
