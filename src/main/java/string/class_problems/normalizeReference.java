52264717cb7b79adcca157068bdbd1ba057b3339
Deep Jain <deepbhai094@gmail.com>
2026-09-11 16:58:32 +0530

    Restore correct Session 2 code

---
+++ src/main/java/string/class_problems/normalizeReference.java
@@ -0,0 +1,40 @@
+package string.class_problems;
+
+import java.util.Scanner;
+
+public class Palindromme {
+
+    static boolean isPalindrome(String text) {
+
+        int start = 0;
+        int end = text.length() - 1;
+
+        while (start < end) {
+
+            if (text.charAt(start) != text.charAt(end)) {
+                return false;
+            }
+
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a word: ");
+        String text = sc.next();
+
+        if (isPalindrome(text)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+
+        sc.close();
+    }
+}
